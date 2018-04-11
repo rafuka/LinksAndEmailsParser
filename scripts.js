a2d116798c835e5c6b49dff064cddbe59e4425ae
Rafael Garcia <rafuk89@gmail.com>
2018-04-11 17:29:07 +0200

    regex working

---
+++ scripts.js
@@ -0,0 +1,42 @@
+
+
+var testInputString = 'Lorem ipsum dolor <a href="mailto:sit@amet.com">site</a> consectetur <span>adipiscing elit</span>, sed do <strong>eiusmod tempor incididunt</strong> ut <a href="http://exampleurl1.com" >labore et</a> dolore <a href="mailto:magna@aliqua.com">Ut</a>. <em>enim ad minim</em> veniam, <a href="#anchor">quis nostrud</a> exercitation ullamco laboris nisi ut <a href="https://exampleurl2.com">aliquip</a> ex ea commodo consequat.</p>';
+
+var returnObject = {
+	links: [],
+	emails: []
+};
+
+var linksPattern = /"(https?:\/\/.+?)"(?:>|.+?>)(.+?)<\/a>/g;
+var emailsPattern = /"mailto:(.+?)"/g;
+var linksArray = [];
+var emailsArray = [];
+
+var match;
+var counter = 0;
+
+while (match = linksPattern.exec(testInputString)) {
+
+	linksArray[counter] = {
+		linkText : match[2],
+		url : match[1]
+	};
+
+	counter++;
+} 
+
+
+while (match = emailsPattern.exec(testInputString)) {
+
+	emailsArray.push(match[1]);
+}
+
+returnObject.links = linksArray;
+returnObject.emails = emailsArray;
+
+
+
+
+
+
+
